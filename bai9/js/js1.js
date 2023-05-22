1eb5b543e630c7e982ca480480d1d626a5061247
luongnguyhen <nguyenluong24052002@gmail.com>
2023-05-22 16:14:42 +0700

    if else và switch trong javascript

---
+++ bai9/js/js1.js
@@ -0,0 +1,27 @@
+        /*
+                let today = 6;
+        if(today == 6) {
+            console.log('phải đi làm')
+        } */
+
+        
+        /*        let date = 6;
+        let today = 'CN'
+
+        if(date = 6 && today != 'CN') {
+            console.log('phải đi làm')
+        } else {
+            console.log('vẫn được nghỉ')
+        }*/
+
+        // let order = 500;
+
+        // if (order > 500) {
+        //     console.log ('bạn được tặng 20%')
+        // } else if (order > 200 && order <= 500) {
+        //     console.log ('được tặng 10%')
+        // } else if (order < 200 && order > 50) {
+        //     console.log ('được tặng 5%')
+        // } else {
+        //     console.log ('bạn không được tặng j')
+        // }
---
+++ bai9/js/js2.js
@@ -0,0 +1,24 @@
+let today = 3   ;
+
+switch(today) {
+    case 2 :
+        console.log ('hôm nay là thứ 2')
+        break;
+    case 3 :
+        console.log ('hôm nay là thứ 3')
+        break;
+    case 4 :
+        console.log ('hôm nay là thứ 4')
+        break;
+    case 5 :
+        console.log ('hôm nay là thứ 5')    
+        break;
+    case 5 :
+        console.log ('hôm nay là thứ 5')    
+        break;
+    case 5 :
+        console.log ('hôm nay là thứ 5')    
+        break;
+        default :
+        console.log ('hôm nay là chủ nhật ')
+}
