05c45a751912730f137b0306526f134235338e0e
luongnguyhen <nguyenluong24052002@gmail.com>
2023-05-26 23:12:45 +0700

    sua loi

--- bai10/js/thuchanh5.js
+++ bai10/js/thuchanh5.js
@@ -1,11 +1,16 @@
-document.write("<table border=1 width=800px>")
-    for(a = 1; a <= 10; a++)       
-    {
-	document.write("<tr>")
-    for(b = 2; b <= 9;b++)   
-    document.write("<td>" +b + "x" + a +"="+ (b * a));
-    document.write("</td>");
-    document.write("</tr>")
+document.write('<table border="1" width="800" cellspacing="0" cellpadding="0">')
+        
+for (let i = 1; i <= 10; i++) {
+    document.write('<tr>')
+
+    for (let y = 2; y <= 10; y++) {
+        document.write('<td>')
+        document.write(`${y} x ${i} = ${i * y}`)
+        document.write('</td>')
     }
-document.write("</table>")
+
+    document.write('</tr>')
+}
+
+document.write('</table>')
  
