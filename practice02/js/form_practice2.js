b3bc83867140eff20f5b063a30010de2770fd16a
luongnguyhen <nguyenluong24052002@gmail.com>
2023-06-12 13:59:57 +0700

    practice1

--- practice02/js/form_practice2.js
+++ practice02/js/form_practice2.js
@@ -1,24 +1,66 @@
-
 let formContainer = document.querySelector('#form-container');
 let inputItem = document.querySelector('.input-item');
 let addbtn = document.querySelector('#add-btn');
-let savebtn = document.querySelector('#save-btn')
+let savebtn = document.querySelector('#save-btn');
+
 
 addbtn.addEventListener('click', function () {
   if (inputItem.childElementCount <= 10) {
     inputItem.insertAdjacentHTML('beforeend', `<div class="input-item">
-  <input type="text">
-  <button onclick="deleteBtn(this)">-</button>
-  </div>`)
-  }else if (inputItem.childElementCount === 11) {
+      <input type="text" placeholder="Nhập thông tin..">
+      <button onclick="deleteBtn(this)">-</button>
+      </div>`);
+  } else if (inputItem.childElementCount === 11) {
     addbtn.style.display = "none";
   }
-})
+});
 
 
-function deleteBtn(button) {
-let inputItem = button.parentNode;
-inputItem.parentNode.removeChild(inputItem);
+savebtn.addEventListener('click', renderListStudent);
+function renderListStudent() {
+  let inputs = inputItem.querySelectorAll('input[type="text"]');
+  let studentList = [];
+
+  inputs.forEach(function (input) {
+    let valueElement = input.value;
+    if (!valueElement) {
+      alert('Vui Lòng Nhập Thông Tin')
+    }else{
+      studentList.push({ content: valueElement, deleted: false });
+    }
+  });
+
+  localStorage.setItem('studentList', JSON.stringify(studentList));
 }
+
+function loadStoredData() {
+  let storedData = localStorage.getItem('studentList');
+  if (storedData) {
+    let studentList = JSON.parse(storedData);
+
+    inputItem.innerHTML = '';
+
+    studentList.forEach(function (student) {
+      inputItem.insertAdjacentHTML('beforeend', `<div class="input-item">
+        <input type="text" placeholder="Nhập thông tin.." value="${student.content}">
+        <button onclick="deleteBtn(this)">-</button>
+      </div>`);
+    });
+
+    if (inputItem.childElementCount >= 11) {
+      addbtn.style.display = "none";
+    }
+  }
+}
+
+function deleteBtn(button){
+  if(confirm('Bạn Có Thật Sự Muốn Xóa Không')) {
+    let inputDiv = button.parentElement;
+    inputDiv.remove();
+    renderListStudent()
+  }
+}
+
+window.addEventListener('load', loadStoredData);
 
 
